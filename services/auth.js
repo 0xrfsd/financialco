f2a932c9e521fd40f9fb61e6bb47d95850d13219
Ricardo Fonseca <64338100+0xrfsd@users.noreply.github.com>
2022-01-18 21:23:42 -0300

    🛠️ services {uri} fixed

--- services/auth.js
+++ services/auth.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 export async function signIn(email, senha) {
   const response = await axios.post(
-    "http://192.168.0.20:5555/api/v0/auth/Login",
+    "http://192.168.0.10:5555/api/v0/auth/Login",
     {
       email: email,
       senha: senha,
@@ -13,7 +13,7 @@ export async function signIn(email, senha) {
 
 export async function signUp(nome, email, cpf, telefone, senha) {
   const response = await axios.post(
-    "http://192.168.0.20:5555/api/v0/auth/Register",
+    "http://192.168.0.10:5555/api/v0/auth/Register",
     {
       nome: nome,
       email: email,
